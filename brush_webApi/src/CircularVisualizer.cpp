58eede8622d83f4c5223bc1d3540fb2ddcbd2648
Hiroshi Matoba <matobahiroshi@gmail.com>
2017-10-20 17:18:32 +0200

    init visualizer class

---
+++ brush_webApi/src/CircularVisualizer.cpp
@@ -0,0 +1,9 @@
+//
+//  CircularVisualizer.cpp
+//  brush_webApi
+//
+//  Created by MatobaHiroshi on 10/20/17.
+//
+//
+
+#include "CircularVisualizer.hpp"
---
+++ brush_webApi/src/CircularVisualizer.hpp
@@ -0,0 +1,14 @@
+//
+//  CircularVisualizer.hpp
+//  brush_webApi
+//
+//  Created by MatobaHiroshi on 10/20/17.
+//
+//
+
+#ifndef CircularVisualizer_hpp
+#define CircularVisualizer_hpp
+
+#include <stdio.h>
+
+#endif /* CircularVisualizer_hpp */
