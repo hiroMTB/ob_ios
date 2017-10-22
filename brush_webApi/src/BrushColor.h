faccce2bfae075ae5f1bfcf607014625f89cc734
Hiroshi Matoba <matobahiroshi@gmail.com>
2017-10-22 19:44:14 +0200

    add brush color class

---
+++ brush_webApi/src/BrushColor.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ofMain.h"
+
+namespace ob{
+
+namespace color{
+    
+    static const ofColor hour  = ofColor( 45, 95,115);
+    static const ofColor day   = ofColor( 83,142,116);
+    static const ofColor week  = ofColor(242,209,179);
+    static const ofColor month = ofColor(45,95,115);
+    static const ofColor year  = ofColor(45,95,115);
+}
+}
