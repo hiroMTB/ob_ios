a5c5bd88b2792bea54dc62f173f79da9cd65f505
hiroMTB <maobahiroshi@gmail.com>
2017-10-31 19:19:36 +0100

    disable depth test

--- brush_webApi/src/main.mm
+++ brush_webApi/src/main.mm
@@ -6,7 +6,7 @@ int main() {
     //------------------------------------------------------
     ofiOSWindowSettings settings;
     settings.enableRetina = true; // enables retina resolution if the device supports it.
-    settings.enableDepth = true; // enables depth buffer for 3d drawing.
+    settings.enableDepth = false; // enables depth buffer for 3d drawing.
     settings.enableAntiAliasing = true; // enables anti-aliasing which smooths out graphics on the screen.
     settings.numOfAntiAliasingSamples = 4; // number of samples used for anti-aliasing.
     settings.enableHardwareOrientation = false; // enables native view orientation.
