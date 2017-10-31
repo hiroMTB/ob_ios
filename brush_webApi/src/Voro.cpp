36f61f31d2a862df69bb31c1c4538cae3ffd90d6
hiroMTB <maobahiroshi@gmail.com>
2017-10-31 19:16:26 +0100

    just add files for Voronoi rendering

---
+++ brush_webApi/src/Voro.cpp
@@ -0,0 +1,75 @@
+#include "Voro.h"
+
+void Voro::create(){
+    // fill vertices
+    
+    vD.clear();
+    boost::polygon::construct_voronoi( vPs.begin(), vPs.end(), &vD);
+}
+
+void Voro::draw(){
+    if( 1 ){
+        ofSetLineWidth(1);
+        voronoi_diagram<double>::const_cell_iterator it = vD.cells().begin();
+        for (; it!= vD.cells().end(); ++it) {
+            
+            const cell_type& cell = *it;
+            const edge_type* edge = cell.incident_edge();
+            
+            do{
+                if(edge->is_primary()){
+                    if( edge->is_finite() ){
+                        if (edge->cell()->source_index() < edge->twin()->cell()->source_index()){
+                            float x0 = edge->vertex0()->x();
+                            float y0 = edge->vertex0()->y();
+                            float x1 = edge->vertex1()->x();
+                            float y1 = edge->vertex1()->y();
+                            
+                            float limity = 800;
+                            float limitx = 4000;
+                            bool xOver = ( abs(x0)>limitx || abs(x1)>limitx );
+                            bool yOver = ( abs(y0)>limity || abs(y1)>limity );
+                            
+                            bool draw = !xOver && !yOver;
+                            
+                            ofSetColor(0,0,0);
+                            
+                            if(draw){
+                                ofDrawLine(x0,y0,0,x1,y1,0);
+                            }else{
+                                if( ofRandom(0.0, 1.0)>0.9){
+                                    ofSetColor(0.5, 0.5, 0.5, 0.5);
+                                    ofDrawLine(x0,y0,0,x1,y1,0);
+                                }
+                            }
+                        }
+                    }else{
+                        if( 0 ){
+                            const vertex_type * v0 = edge->vertex0();
+                            if( v0 ){
+                                vPoint p1 = vPs[edge->cell()->source_index()];
+                                vPoint p2 = vPs[edge->twin()->cell()->source_index()];
+                                float x0 = edge->vertex0()->x();
+                                float y0 = edge->vertex0()->y();
+                                float end_x = (p1.y() - p2.y()) * ofGetWidth();
+                                float end_y = (p1.x() - p2.x()) * -ofGetWidth();
+                                ofDrawLine(x0,y0,0,end_x,end_y,0);
+                            }
+                        }
+                    }
+                }
+                edge = edge->next();
+            }while (edge != cell.incident_edge());
+        }
+    }
+    
+    // draw Point
+    if( 0 ){
+        glPointSize(3);
+        ofSetColor(255, 255, 255, 200);
+        for( auto v : vPs ){
+            ofDrawCircle(v.x(), v.y(), 2);
+        }
+    }
+    
+}
---
+++ brush_webApi/src/Voro.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+#include "boost/polygon/voronoi.hpp"
+#include "boost/polygon/segment_data.hpp"
+
+using namespace std;
+using boost::polygon::voronoi_builder;
+using boost::polygon::voronoi_diagram;
+typedef boost::polygon::point_data<float> vPoint;
+typedef boost::polygon::segment_data<float> vSegment;
+typedef voronoi_diagram<double>::cell_type cell_type;
+typedef voronoi_diagram<double>::edge_type edge_type;
+typedef voronoi_diagram<double>::vertex_type vertex_type;
+
+class Voro{
+    
+public:
+    
+    Voro(){};
+    
+    void create();
+    void draw();
+    
+    vector<vPoint> vPs;
+    voronoi_diagram<double> vD;
+    
+};
